561fae8bc2eab56ddf67a8e47eeb7ae0434dec35
hmnsakib <hmnazmussakib05@gmail.com>
2025-09-23 23:41:51 +0600

    Initial commit for empty project

---
+++ README.md
@@ -0,0 +1,3 @@
+# SeedsOfSalvation
+
+Developed with Unreal Engine 5
---
+++ Source/SeedsOfSalvation/SeedsOfSalvation.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SeedsOfSalvation.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SeedsOfSalvation, "SeedsOfSalvation" );
