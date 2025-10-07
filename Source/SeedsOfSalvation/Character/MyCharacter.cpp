33183a37e0a8480431bf3825899078a2b7edc6d3
Sadman Atul <145335591+SadmanAtul@users.noreply.github.com>
2025-10-07 17:24:31 +0600

    character code update and model of room fix
    character code update and model of room fix

--- Source/SeedsOfSalvation/Character/MyCharacter.cpp
+++ Source/SeedsOfSalvation/Character/MyCharacter.cpp
@@ -1,93 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #include "MyCharacter.h"
-#include "Camera/CameraComponent.h"
+#include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "Components/CapsuleComponent.h"
 
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
-    PrimaryActorTick.bCanEverTick = true;
+	// Enable Tick
+	PrimaryActorTick.bCanEverTick = true;
 
-    // Capsule size
-    GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+	// Capsule setup
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-    // Disable controller rotation, let character rotate with movement
-    bUseControllerRotationPitch = false;
-    bUseControllerRotationYaw = false;
-    bUseControllerRotationRoll = false;
+	// Disable controller rotation
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 
-    // Configure character movement
-    GetCharacterMovement()->bOrientRotationToMovement = true;
-    GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
-    GetCharacterMovement()->JumpZVelocity = 400.f;
-    GetCharacterMovement()->AirControl = 0.2f;
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 540.f, 0.f);
+	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->AirControl = 0.2f;
 
-    // Create camera boom
-    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-    CameraBoom->SetupAttachment(RootComponent);
-    CameraBoom->TargetArmLength = 300.f;
-    CameraBoom->bUsePawnControlRotation = true;
+	// Create camera boom
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.f; // Camera follows at this distance
+	CameraBoom->bUsePawnControlRotation = true;
 
-    // Create follow camera
-    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-    FollowCamera->bUsePawnControlRotation = false;
+	// Create follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
 
-    // Character starts alive
-    bDead = false;
+	// Initialize variables
+	bDead = false;
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 }
 
 // Called every frame
 void AMyCharacter::Tick(float DeltaTime)
 {
-    Super::Tick(DeltaTime);
+	Super::Tick(DeltaTime);
 }
 
 // Called to bind functionality to input
 void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-    Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-    // Movement bindings
-    PlayerInputComponent->BindAxis("MoveForward", this, &AMyCharacter::MoveForward);
-    PlayerInputComponent->BindAxis("MoveRight", this, &AMyCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 
-    // Camera control
-    PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-    PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
-    // Jump
-    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
-    PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMyCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMyCharacter::MoveRight);
 }
 
 void AMyCharacter::MoveForward(float Axis)
 {
-    if (!bDead && Controller && Axis != 0.0f)
-    {
-        const FRotator Rotation = Controller->GetControlRotation();
-        const FRotator YawRotation(0, Rotation.Yaw, 0);
+	if (Controller && Axis != 0.0f && !bDead)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
-        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-        AddMovementInput(Direction, Axis);
-    }
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Axis);
+	}
 }
 
 void AMyCharacter::MoveRight(float Axis)
 {
-    if (!bDead && Controller && Axis != 0.0f)
-    {
-        const FRotator Rotation = Controller->GetControlRotation();
-        const FRotator YawRotation(0, Rotation.Yaw, 0);
+	if (Controller && Axis != 0.0f && !bDead)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
 
-        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-        AddMovementInput(Direction, Axis);
-    }
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Axis);
+	}
 }
--- Source/SeedsOfSalvation/Character/MyCharacter.h
+++ Source/SeedsOfSalvation/Character/MyCharacter.h
@@ -5,7 +5,13 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Blueprint/UserWidget.h"
 #include "MyCharacter.generated.h"
 
 UCLASS()
@@ -14,21 +20,8 @@ class SEEDSOFSALVATION_API AMyCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-
+	// Sets default values for this character's properties
 	AMyCharacter();
-
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
-	USpringArmComponent* CameraBoom;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
-	UCameraComponent* FollowCamera;
-
-	void MoveForward(float Axis);
-	void MoveRight(float Axis);
-
-	UPROPERTY(BlueprintReadWrite, Category = "State")
-	bool bDead;
 
 protected:
 	// Called when the game starts or when spawned
@@ -40,4 +33,19 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Camera components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	UCameraComponent* FollowCamera;
+
+	// Movement functions
+	void MoveForward(float Axis);
+	void MoveRight(float Axis);
+
+	// Example state variable
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Character State")
+	bool bDead;
 };
